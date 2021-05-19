d50668fb8d5e1d91a9c51bb2708ec351f9c225f9
TylerT <alpinetyler@gmail.com>
2021-05-18 20:16:06 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MonthlyPayment from './components/MonthlyPayment';
 import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Form />
+      <MonthlyPayment />
+      <Footer />
     </div>
   );
 }
