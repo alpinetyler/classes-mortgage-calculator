d900d373e7d032c4412222ff0d5588af2a7e976d
TylerT <alpinetyler@gmail.com>
2021-05-24 08:34:25 -0600

    updates to Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
 
-
 export default class Form extends Component {
+
+    constructor() {
+        super()
+    
+        this.state = {
+            amount: ""
+        }
+    }
+    
+    handleChange(value) {
+        let newValue = value * 2;
+    
+        this.setState({ amount: newValue})
+    }
     
     render() {
         return(
             <div className="App">
                 <h1>Hello Form!</h1>
+                <form automcomplete="off">
+                <input 
+                name="amount" 
+                onChange={e => this.handleChange(e.target.value)}></input>
+                </form>
+            new Amount = {this.state.amount}
             </div>
         )
     }
