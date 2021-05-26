48d3521eb0b54f1edf8426d7fc92f8bd67d830bc
alpinetyler <49990880+alpinetyler@users.noreply.github.com>
2021-05-25 19:02:20 -0600

    Form changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,26 +6,61 @@ export default class Form extends Component {
         super()
     
         this.state = {
-            amount: ""
+            amount: "",
+            rate: "",
+            years: ""
         }
     }
     
-    handleChange(value) {
-        let newValue = value * 2;
+    handleChange = (event) => {
     
-        this.setState({ amount: newValue})
+       const {name, value} = event
+      
+           this.setState({ [name]: value})  
+    
     }
     
     render() {
+        let {amount, rate, years} = this.state
+        parseFloat(amount, rate, years)
+    
+        let monthlyInterest = (rate / 100) / 12;
+        let months = years * 12;
+        
+        amount = amount.replace(/\$|,/g, '')
+    
+        let monthlyPayment = (amount * monthlyInterest) / (1 - Math.pow(1 + monthlyInterest, -months))
         return(
+
+            
             <div className="App">
-                <h1>Hello Form!</h1>
-                <form automcomplete="off">
+                <h1>The Form</h1>
+                <form automomplete="off">
                 <input 
                 name="amount" 
-                onChange={e => this.handleChange(e.target.value)}></input>
+                value={this.state.amount}
+                onChange={e => this.handleChange(e.target)} 
+                autocomplete="off"
+                placeholder="Loan Amount"
+                />
+                <p>
+                <input 
+                name="rate" 
+                value={this.state.rate}
+                onChange={e => this.handleChange(e.target)} 
+                autocomplete="off"
+                placeholder="Interest Rate"/>
+                </p>
+                <p>
+                <input 
+                name="years" 
+                value={this.state.years}
+                onChange={e => this.handleChange(e.target)} 
+                autocomplete="off"
+                placeholder="Term in years"/>
+                </p>
                 </form>
-            new Amount = {this.state.amount}
+            Monthly Payment = {monthlyPayment}
             </div>
         )
     }
